9e81904708f8fc2b172e245a7698e758d221bd36
YG THOR <51053902+ygthor@users.noreply.github.com>
2025-04-19 22:56:03 +0800

    x

--- electron/main.js
+++ electron/main.js
@@ -1,34 +1,38 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-require('electron-reload')(__dirname, {
-    electron: require(`${__dirname}/../node_modules/electron`)
-});
+// Only reload in development
+if (!app.isPackaged) {
+  require('electron-reload')(__dirname, {
+    electron: require(path.join(__dirname, '../node_modules/electron'))
+  });
+}
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1000,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
 
-    // For development (Vite or Vue CLI dev server)
-    // win.loadURL('http://localhost:5173'); // or http://localhost:8080 for Vue CLI
+  if (!app.isPackaged) {
+    // Development: load Vue CLI dev server
     win.loadURL('http://localhost:8080');
-
-    // For production
-    // win.loadFile(path.join(__dirname, '../dist/index.html'));
+  } else {
+    // Production: load the built Vue app
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
